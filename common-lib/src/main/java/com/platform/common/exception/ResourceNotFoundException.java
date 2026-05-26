db3ffb8382a0b7490172e29f38f57791f1804cf4
yafaaa <yafetmichael36@gmail.com>
2026-05-26 20:08:47 +0300

    Initial commit: Microservices backend with Vite frontend and native file opening support

--- common-lib/src/main/java/com/platform/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.platform.common.exception;
-
-public class ResourceNotFoundException extends PlatformException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
